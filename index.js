abee293a99a946cc7c9de031fcc5142ca4a8e112
Chris Di Luca <chrisdiluca@Chriss-MacBook-Pro.local>
2024-01-16 18:24:31 +0000

    added questions array, added function to write readme file, added function to initialize program

--- index.js
+++ index.js
@@ -5,17 +5,70 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Provide a description of your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Provide installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide usage information:',
+  },
+  {
+    type: 'input',
+    name: 'contribution',
+    message: 'Provide contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Provide test instructions:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'GPL', 'Apache', 'ISC', /* Add more if needed */],
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
-}
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+  }
 
 // function to initialize program
 function init() {
-
-}
+    inquirer
+      .prompt(questions)
+      .then((userResponses) => {
+        const markdown = generateMarkdown(userResponses);
+        return writeToFile("README.md", markdown);
+      })
+      .then(() => console.log("README.md successfully generated!"))
+      .catch((err) => console.error("Error generating README:", err));
+  }
 
 // function call to initialize program
 init();
