0db871cc6a07a37fddf111b794cb3361605488ab
Chris Di Luca <chrisdiluca@Chriss-MacBook-Pro.local>
2024-01-16 18:40:38 +0000

    added extra functions for generating the md file

--- README.md
+++
@@ -1,2 +0,0 @@
-# readme-generator-app
-this repo is for practicing at making a command line application that accepts user input. * When a user is prompted for information about the application repository then a high-quality, professional README.md is generated
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contribution}
+
+## Tests
+${data.tests}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Questions
+GitHub: [${data.githubUsername}](https://github.com/${data.githubUsername})
+Email: ${data.email}
 `;
 }
 
