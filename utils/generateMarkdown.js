84ac7380383d7942c0509b3ee2498fc4e9a1470b
Chris Di Luca <chrisdiluca@Chriss-MacBook-Pro.local>
2024-01-16 19:13:35 +0000

    edit redme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,8 +11,7 @@ ${data.description}
 - [Usage](#usage)
 - [Contributing](#contributing)
 - [Tests](#tests)
-- [License](#license)
-- [Questions](#questions)
+
 
 ## Installation
 ${data.installation}
